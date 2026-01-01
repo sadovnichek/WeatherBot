e1cf1069ae2cccefc49e120f05ef28c71f11c5d1
sadovnichek <kapkaevictor@gmail.com>
2026-01-01 18:51:26 +0500

    Add Dockerfile

--- WeatherBotWebApi/Program.cs
+++ WeatherBotWebApi/Program.cs
@@ -2,10 +2,11 @@ using WeatherBotDomain;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var port = Environment.GetEnvironmentVariable("PORT");
+builder.WebHost.UseUrls($"http://0.0.0.0:{port}");
 var app = builder.Build();
 
 var token = Environment.GetEnvironmentVariable("BOT_TOKEN");
-
 var bot = new WeatherBot(token);
 
 app.MapPost("/webhook", async (CustomerUpdate u) =>
