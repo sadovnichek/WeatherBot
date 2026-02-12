40f8569b3189cd83f983f712df42d8b54227961d
sadovnichek <kapkaevictor@gmail.com>
2026-02-12 14:16:04 +0500

    hotfix: correct markdown bold style

--- WeatherBotDomain/Reply/CompoundWeatherReply.cs
+++ WeatherBotDomain/Reply/CompoundWeatherReply.cs
@@ -10,12 +10,12 @@
         {
             var delimiter = WeathersWithoutWording.Length > 0 && WeathersWithWording.Length > 0 ? ", а также " : string.Empty;
             var wording = WeathersWithWording.Length > 0 ? " погода" : string.Empty;
-            var withWording = string.Join(" и ", WeathersWithWording.Select(w => $"**{w}**"));
-            var withoutWording = string.Join(", ", WeathersWithoutWording.Select(w => $"**{w}**"));
+            var withWording = string.Join(" и ", WeathersWithWording.Select(w => $"*{w}*"));
+            var withoutWording = string.Join(", ", WeathersWithoutWording.Select(w => $"*{w}*"));
             
             return $"{Greeting} {TimePointer} ожидаются {withWording}{wording}{delimiter}{withoutWording} {Emoji}\n" +
-                        $"Средняя температура днем: **{MedianTemperature}°C**.\n" +
-                        $"Перепады температур в течении суток с **{MinTemperature}°C** до **{MaxTemperature}°C**";
+                        $"Средняя температура днем: *{MedianTemperature}°C*.\n" +
+                        $"Перепады температур в течении суток с *{MinTemperature}°C* до *{MaxTemperature}°C*";
         }
     }
 }
