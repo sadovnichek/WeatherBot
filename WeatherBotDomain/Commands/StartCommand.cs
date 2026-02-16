60e183027576a80d4282bfd8a605bab26e8764fe
sadovnichek <kapkaevictor@gmail.com>
2026-02-16 12:28:59 +0500

    fix typo in word tomorrow

--- WeatherBotDomain/Commands/StartCommand.cs
+++ WeatherBotDomain/Commands/StartCommand.cs
@@ -19,7 +19,7 @@ namespace WeatherBotDomain.Commands
             await messageBus.WriteAsync("""
                 Добро пожаловать в WeatherBot! 
                 Бот умеет давать прогноз погоды, температуру воздуха и информацию об осадках на сегодня/завтра.
-                Достаточно ввести команды /today или /tommorrow.
+                Достаточно ввести команды /today или /tomorrow.
                 Больше команд доступны в /help
                 """);
         }
