a9732a76bd0d3c634676f5ae27c512ac1b9c38cf
sadovnichek <kapkaevictor@gmail.com>
2026-02-26 11:25:20 +0500

    rename domain classes

---
+++ WeatherBotDomain/WeatherType.cs
@@ -0,0 +1,15 @@
+﻿namespace WeatherBotDomain
+{
+    public enum WeatherType
+    {
+        Sunny,
+        PartlyCloudy,
+        Cloudy,
+        Rainy,
+        Fog,
+        SlightlyRainy,
+        Snowy,
+        Shower,
+        Thunderstorm
+    }
+}
