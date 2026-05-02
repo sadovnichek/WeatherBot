42af9a930cb0f9261158637198daea23fb7c7e9a
sadovnichek <kapkaevictor@gmail.com>
2026-05-02 12:41:57 +0500

    add Reply.cs

---
+++ WeatherBotInfrastructure/Reply.cs
@@ -0,0 +1,23 @@
+﻿namespace BotInfrastructure
+{
+    public abstract class Reply : IReply
+    {
+        public Reply? Next { get; private set; }
+
+        public bool HasNext => Next != null;
+
+        public Reply Root { get; private set; }
+
+        public abstract string BuildMessage();
+
+        public Reply FollowWith(Reply reply)
+        {
+            if (Root == null)
+                Root = this;
+
+            Next = reply;
+            reply.Root = Root;
+            return Root;
+        }
+    }
+}
