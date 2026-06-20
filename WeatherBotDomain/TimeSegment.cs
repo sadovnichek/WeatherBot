052d3ff65d4cc2218971f17bfbdbe85cfd6d78c2
sadovnichek <kapkaevictor@gmail.com>
2026-06-20 19:34:03 +0500

    add time segment join method

--- WeatherBotDomain/TimeSegment.cs
+++ WeatherBotDomain/TimeSegment.cs
@@ -20,5 +20,29 @@
         {
             return time.IsBetween(Start, End);
         }
+
+        public static IEnumerable<TimeSegment> Join(TimeSegment[] segments)
+        {
+            if (segments.Length == 0)
+                yield break;
+
+            var sorted = segments.OrderBy(s => s.Start).ToArray();
+            var currentStart = sorted[0].Start;
+            var currentEnd = sorted[0].End;
+
+            for (var i = 1; i < sorted.Length; i++)
+            {
+                if (sorted[i].Start > currentEnd)
+                {
+                    yield return new TimeSegment(currentStart, currentEnd);
+                    currentStart = sorted[i].Start;
+                    currentEnd = sorted[i].End;
+                }
+                if (sorted[i].End > currentEnd)
+                    currentEnd = sorted[i].End;
+            }
+
+            yield return new TimeSegment(currentStart, currentEnd);
+        }
     }
 }
---
+++ WeatherBotDomainTests/TimeSegmnet_should.cs
@@ -0,0 +1,21 @@
+﻿using WeatherBotDomain;
+
+namespace WeatherBotDomainTests
+{
+    [TestFixture]
+    public class TimeSegmnet_shouldcs
+    {
+        [Test]
+        public void Test1()
+        {
+            var t1 = new TimeSegment(new TimeOnly(0, 0), new TimeOnly(4, 0));
+            var t2 = new TimeSegment(new TimeOnly(6, 0), new TimeOnly(8, 0));
+            var t3 = new TimeSegment(new TimeOnly(1, 0), new TimeOnly(2, 0));
+            var t4 = new TimeSegment(new TimeOnly(3, 0), new TimeOnly(9, 0));
+            var t5 = new TimeSegment(new TimeOnly(11, 0), new TimeOnly(13, 0));
+            var t6 = new TimeSegment(new TimeOnly(12, 0), new TimeOnly(14, 0));
+
+            var result = TimeSegment.Join(new[] { t1, t2, t3, t4, t5, t6 }).ToList();
+        }
+    }
+}
